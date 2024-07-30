6d0cbe59aaf7d2205539c842fe97622690d61eb9
Wisnu Satrio <batarawisnu96@gmail.com>
2024-07-30 07:01:53 +0700

    fix/api url

--- frontend/config/apiConfig.js
+++ frontend/config/apiConfig.js
@@ -2,8 +2,8 @@ export const API_CONFIG = {
     baseUrl: 'https://your-api-domain.com',
     endpoints: {
         coinData: '/api/coins',
-        auth: 'https://b764-125-160-193-58.ngrok-free.app/api/auth/',
-        register: 'https://b764-125-160-193-58.ngrok-free.app/api/user/'
+        auth: 'https://1bfb-36-85-74-78.ngrok-free.app/api/auth/',
+        register: 'https://1bfb-36-85-74-78.ngrok-free.app/api/user/'
         // Add other endpoints as needed
     }
 };
