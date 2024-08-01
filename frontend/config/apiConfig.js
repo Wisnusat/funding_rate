f5493eeb80efc06b3df6a5ce74b519ff8cc69c74
Wisnu Satrio <batarawisnu96@gmail.com>
2024-08-01 12:03:04 +0700

    fix/change base url

--- frontend/config/apiConfig.js
+++ frontend/config/apiConfig.js
@@ -2,9 +2,9 @@ export const API_CONFIG = {
     baseUrl: 'https://your-api-domain.com',
     endpoints: {
         coinData: '/api/coins',
-        auth: 'https://1bfb-36-85-74-78.ngrok-free.app/api/auth/',
-        register: 'https://1bfb-36-85-74-78.ngrok-free.app/api/user/',
-        aevo: (page, limit) => `https://1bfb-36-85-74-78.ngrok-free.app/api/funding-rates/aevo?page=${page}&limit=${limit}&time=1h`,
+        auth: 'http://shiny-adelaida-suryalab-d961bd87.koyeb.app/api/auth/',
+        register: 'http://shiny-adelaida-suryalab-d961bd87.koyeb.app/api/user/',
+        aevo: (page, limit) => `http://shiny-adelaida-suryalab-d961bd87.koyeb.app/api/funding-rates/aevo?page=${page}&limit=${limit}&time=1h`,
         // Add other endpoints as needed
     }
 };
