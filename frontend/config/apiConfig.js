017a22bc17432d63bd6258ac20093a477fb2b78e
Wisnu Satrio <batarawisnu96@gmail.com>
2025-02-11 04:10:29 +0700

    fix/change base url

--- frontend/config/apiConfig.js
+++ frontend/config/apiConfig.js
@@ -1,4 +1,4 @@
-const baseUrl = 'https://decent-mosquito-surya-2-40873c23.koyeb.app';
+const baseUrl = 'https://48c1-125-163-131-158.ngrok-free.app';
 const coinMarketCapApiKey = 'd1b90582-1f85-4dee-8c31-2d43c234a453';
 
 export const API_CONFIG = {
